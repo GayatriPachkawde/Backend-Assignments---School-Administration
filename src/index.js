29c183a9b0157534c1fe0f2105a90b4973dc96ea
GayatriPachkawde <gayatri5kawde@gmail.com>
2020-12-16 12:55:59 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,45 +1,37 @@
-// const mongoose = require("mongoose");
-
-// const marioSchema = new mongoose.Schema({
-//   name: String,
-//   weight: Number,
-// });
-
-// //
-// //module.exports = mongoose.model("marioModel", marioModel);
-// const marioModel = mongoose.model("mariochar", marioSchema);
-// module.exports = marioModel;
-
 const express = require("express");
 const app = express();
+app.use(express.json());
+const studentArray = require("./InitialData.js");
 const bodyParser = require("body-parser");
 const port = 8080;
-const fs = require("fs");
-var studentArray = require("./InitialData");
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // your code goes here
+
 app.get("/api/student", (req, res) => {
   res.send(studentArray);
 });
 app.get("/api/student/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (id >= 1 && id <= 7) {
-    res.send({
-      id: studentArray[id - 1].id,
-      name: studentArray[id - 1].name,
-      currentClass: studentArray[id - 1].currentClass,
-      division: studentArray[id - 1].division,
-    });
-  } else {
+  const object = studentArray[req.params.id - 1];
+  if (typeof object === "undefined") {
     res.sendStatus(404);
+  } else {
+    res.send(object);
   }
 });
-
+app.delete("/api/student/:id", (req, res) => {
+  const object = studentArray[req.params.id - 1];
+  if (typeof object === "undefined") {
+    res.sendStatus(404);
+  } else {
+    delete studentArray[req.params.id - 1];
+    res.sendStatus(200);
+  }
+});
 app.post("/api/student", (req, res) => {
   if (req.body.name && req.body.currentClass && req.body.division) {
     const id = studentArray.length + 1;
@@ -57,10 +49,12 @@ app.post("/api/student", (req, res) => {
     res.sendStatus(400);
   }
 });
-
+const nullOrUndefined = (variable) => {
+  return variable === null || variable === undefined ? true : false;
+};
 app.put("/api/student/:id", (req, res) => {
   const object = studentArray[req.params.id - 1];
-  if (!object) {
+  if (nullOrUndefined(object)) {
     res.sendStatus(400);
   } else {
     if (req.body.name || req.body.currentClass || req.body.division) {
@@ -79,16 +73,6 @@ app.put("/api/student/:id", (req, res) => {
     }
   }
 });
-app.delete("/api/student/:id", (req, res) => {
-  const object = studentArray[req.params.id - 1];
-  if (typeof object === "undefined") {
-    res.sendStatus(404);
-  } else {
-    delete studentArray[req.params.id - 1];
-    res.sendStatus(200);
-  }
-});
-
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
